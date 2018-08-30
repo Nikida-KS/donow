f8b9d9eec2b51b377abee441384655250aa9c75f
Zachary Sexton <zacharysexton@Zacharys-MacBook-Pro-2.local>
2018-08-30 18:38:38 -0500

    initial commit

---
+++ src/toDoList.js
@@ -0,0 +1,23 @@
+'use strict'
+import React from 'react';
+
+export default class TodoList extends React.Component {
+  state = {
+
+  }
+
+  render (){
+    return (
+      <div>
+        <h2>To Do List</h2>
+        <div style = {{}}
+      </div>
+    )
+  }
+}
+
+/*delete function:
+1. Add a checkbox to each item
+2. Add a delete button that deletes all of the items that are checked
+  Use .filter to return all of the items that are not checked
+*/
